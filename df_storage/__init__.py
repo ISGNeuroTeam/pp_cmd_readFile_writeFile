dfece4a18cdabf2015dd9d3c90c0c9a8f94ffc07
Artem Zenkov <azenkov@isgneuro.com>
2023-04-05 20:44:27 +0700

    add syntax with format and path keywords

--- df_storage/__init__.py
+++ df_storage/__init__.py
@@ -1,1 +1,2 @@
 from .df_storage import DfStorage, WriteMode
+from .parse_args import get_read_write_args
---
+++ df_storage/parse_args.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+
+def get_read_write_args(
+        command_config, path_arg, storage_arg, filename_arg, type_arg, format_arg
+):
+    if 'storages' in command_config:
+        storages = command_config['storages']
+    else:
+        storages = {}
+
+    if 'lookups' not in storages:
+        storages['lookups'] = '/opt/otp/lookups'
+    if 'external_data' not in storages:
+        storages['external_data'] = '/opt/otp/external_data'
+
+    if path_arg and filename_arg:
+        raise ValueError('Path and filename arguments passed. Need only one of them')
+
+    if path_arg:
+        filename = path_arg
+        file_type = format_arg
+    elif filename_arg is not None:
+        filename = filename_arg
+        file_type = type_arg
+    else:
+        raise ValueError('Need path argument')
+
+    if storage_arg is None:
+        storage_arg = 'external_data'
+
+    if storage_arg not in storages:
+        raise ValueError('Unknown storage')
+
+    storage_path = storages[storage_arg]
+    Path(storage_path).mkdir(exist_ok=True, parents=True)
+
+    return storage_path, filename, file_type
